c8f3613c6390a9fc0710d2a8226d46bf6b7110ba
samuvel6826 <samuvel6826@gmail.com>
2025-12-21 23:25:06 +0530

    chore: renewed

--- backend/nodejs-server/config/firebaseConfig.js
+++ backend/nodejs-server/config/firebaseConfig.js
@@ -8,10 +8,19 @@ const initializeFirebase = () => {
         try {
             let serviceAccount;
 
-            if (process.env.RENDER === 'true') {
+            // Check if running on Render platform
+            if (process.env.RENDER && process.env.RENDER === 'true') {
+                // Production: Load from Render secrets
+                logger.info('Loading Firebase credentials from Render secrets');
                 serviceAccount = require('/etc/secrets/serviceAccountKey.json');
+            } else if (process.env.FIREBASE_SERVICE_ACCOUNT_KEY) {
+                // Environment variable: Parse JSON string
+                logger.info('Loading Firebase credentials from environment variable');
+                serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
             } else {
-                serviceAccount = process.env.FIREBASE_SERVICE_ACCOUNT_KEY;
+                // Local development: Load from file
+                logger.info('Loading Firebase credentials from local file');
+                serviceAccount = require('../serviceAccountKey.json');
             }
 
             firebaseApp = admin.initializeApp({
